b0b2054014a8e2af98b1c1c2f3a7e2e4ccf79f19
Daniel Bauer <dan717@gmx.de>
2023-02-21 11:43:43 +0100

    Recap arrays

--- 02. A - Optional - JavaScript - A Quick Refresher/012. L - Refreshing the Core Syntax M/play.js
+++ 02. A - Optional - JavaScript - A Quick Refresher/012. L - Refreshing the Core Syntax M/play.js
@@ -1,3 +1,4 @@
+// Objects:
 const person = {
   name: "Max",
   age: 29,
@@ -5,4 +6,13 @@ const person = {
     console.log("Hi, i am " + this.name)
   }
 };
-person.greet();
+//person.greet();
+
+// Arrays:
+const hobbies = ["Sports", "Cooking",];
+
+for (let hobby of hobbies) {
+  console.log(hobby);
+}
+console.log(hobbies.map(hobby => "My hobby is: " + hobby));
+console.log(hobbies);
