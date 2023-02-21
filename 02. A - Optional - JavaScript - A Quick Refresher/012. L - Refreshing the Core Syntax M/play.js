28ce7e593fdc79066a8e261700ca0bd8a2421fee
Daniel Bauer <dan717@gmx.de>
2023-02-21 14:46:28 +0100

    Recap destructuring

--- 02. A - Optional - JavaScript - A Quick Refresher/012. L - Refreshing the Core Syntax M/play.js
+++ 02. A - Optional - JavaScript - A Quick Refresher/012. L - Refreshing the Core Syntax M/play.js
@@ -8,25 +8,33 @@ const person = {
 };
 //person.greet();
 
-const copiedPerson = {...person};
-console.log(copiedPerson);
-
-
-
-// Arrays:
-const hobbies = ["Sports", "Cooking",];
-hobbies.push("Programming");
-
-const copiedArray = [...hobbies];
-//console.log(copiedArray);
-
-
-// for (let hobby of hobbies) {
-//   console.log(hobby);
-// }
-// console.log(hobbies.map(hobby => "My hobby is: " + hobby));
-
-const toArray = (...args) => {
-  return args;
+const printName = ({ name }) => {
+  //console.log(name);
 };
-console.log(toArray(1, 2, 3, 4));
+printName(person);
+
+const { name, age } = person;
+
+// const copiedPerson = {...person};
+// console.log(copiedPerson);
+
+// // Arrays:
+const hobbies = ["Sports", "Cooking"];
+const [hobby1, hobby2] = hobbies;
+console.log(hobby1, hobby2);
+
+// hobbies.push("Programming");
+
+// const copiedArray = [...hobbies];
+// //console.log(copiedArray);
+
+
+// // for (let hobby of hobbies) {
+// //   console.log(hobby);
+// // }
+// // console.log(hobbies.map(hobby => "My hobby is: " + hobby));
+
+// const toArray = (...args) => {
+//   return args;
+// };
+// console.log(toArray(1, 2, 3, 4));
