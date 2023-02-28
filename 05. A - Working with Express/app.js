ea3313042c16e83161054a94fa17d6dc96af5457
Daniel Bauer <dan717@gmx.de>
2023-02-28 11:16:03 +0100

    Done using express router

--- 05. A - Working with Express/app.js
+++ 05. A - Working with Express/app.js
@@ -1,23 +1,20 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+// Making use of express
 const app = express();
+
+// Importing the Admin Routes:
+const adminRoutes = require("./routes/admin");
+// Importing the Shop Routes:
+const shopRoutes = require("./routes/shop");
 
 // Middleware Parsing:
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// Middleware Routes:
-app.use("/add-product", (req, res, next) => {
-  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-});
-
-app.post("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  res.send("<h1>Hello from Express.js!</h1>");
-});
+// Makingg use of Route Object adminRoutes:
+app.use(adminRoutes);
+// Makingg use of Route Object shopRoutes:
+app.use(shopRoutes);
 
 app.listen(3000);
