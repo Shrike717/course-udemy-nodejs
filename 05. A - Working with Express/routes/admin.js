11136690efaacd9541fa59285a0aa35bd0fa8db0
Daniel Bauer <dan717@gmx.de>
2023-02-28 15:21:18 +0100

    Added 404 page and returned it

--- 05. A - Working with Express/routes/admin.js
+++ 05. A - Working with Express/routes/admin.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 const express = require("express");
 
 // Making use of Router Module:
@@ -5,7 +7,7 @@ const router = express.Router();
 
 // Middleware Routes: /admin/add-product. Watch Filter!
 router.get("/add-product", (req, res, next) => {
-  res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
 
 router.post("/add-product", (req, res, next) => {
