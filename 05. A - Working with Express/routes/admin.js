44e4f50f8ccf04b2091b46a46926f1e3cd81748f
Daniel Bauer <dan717@gmx.de>
2023-03-01 11:36:47 +0100

    Done implemeenting helper function to root directory

--- 05. A - Working with Express/routes/admin.js
+++ 05. A - Working with Express/routes/admin.js
@@ -2,12 +2,14 @@ const path = require("path");
 
 const express = require("express");
 
+const rootDir = require("../util/path");
+
 // Making use of Router Module:
 const router = express.Router();
 
 // Middleware Routes: /admin/add-product. Watch Filter!
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 router.post("/add-product", (req, res, next) => {
