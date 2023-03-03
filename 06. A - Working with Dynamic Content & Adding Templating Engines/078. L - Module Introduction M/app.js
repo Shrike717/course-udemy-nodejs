615a7fb2ec12bef24d4c72e19aca94f0e33720a6
Daniel Bauer <dan717@gmx.de>
2023-03-03 10:28:33 +0100

    Added main layout file and extended other pages

--- 06. A - Working with Dynamic Content & Adding Templating Engines/078. L - Module Introduction M/app.js
+++ 06. A - Working with Dynamic Content & Adding Templating Engines/078. L - Module Introduction M/app.js
@@ -28,7 +28,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 // Catch-All Middleware for errors:
 app.use((req, res, next) => {
-  res.status(404).render("404");
+  res.status(404).render("404", { pageTitle: "Page not found" });
 });
 
 app.listen(3000);
