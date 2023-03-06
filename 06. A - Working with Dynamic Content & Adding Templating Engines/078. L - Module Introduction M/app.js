ef96e829cd88df8138c2926157581f82742d5b70
Daniel Bauer <dan717@gmx.de>
2023-03-06 15:08:57 +0100

    Done implementing partials

--- 06. A - Working with Dynamic Content & Adding Templating Engines/078. L - Module Introduction M/app.js
+++ 06. A - Working with Dynamic Content & Adding Templating Engines/078. L - Module Introduction M/app.js
@@ -28,7 +28,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 // Catch-All Middleware for errors:
 app.use((req, res, next) => {
-  res.status(404).render("404", { pageTitle: "Page Not Found" });
+  res.status(404).render("404", { pageTitle: "Page Not Found", path: "" });
 });
 
 app.listen(3000);
