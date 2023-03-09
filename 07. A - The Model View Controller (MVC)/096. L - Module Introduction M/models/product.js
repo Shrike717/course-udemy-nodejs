749417b9769c15cd1468756eea7a3a825dce9556
Daniel Bauer <dan717@gmx.de>
2023-03-09 10:31:38 +0100

    Done refactoring storage code

--- 07. A - The Model View Controller (MVC)/096. L - Module Introduction M/models/product.js
+++ 07. A - The Model View Controller (MVC)/096. L - Module Introduction M/models/product.js
@@ -1,22 +1,36 @@
 const fs = require("fs");
 const path = require("path");
 
+// Defines path to file where we save globally
+const p = path.join(
+  path.dirname(require.main.filename),
+  "data",
+  "products.json"
+);
+
+// Helper function to read save file.
+// Returns empty array in case there is no file
+// Otherwise returns array with products in it
+const getProductsFromFile = (cb) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
+
 module.exports = class Product {
   constructor(title) {
     this.title = title;
   }
 
+  // The passed CB function gets either empty array or aray with products from helper function above
+  // Then pushes new product in this array
+  // Then writes new array to products.json file.
   save() {
-    const p = path.join(
-      path.dirname(require.main.filename),
-      "data",
-      "products.json"
-    );
-    fs.readFile(p, (err, fileContent) => {
-      let products = [];
-      if (!err) {
-        products = JSON.parse(fileContent);
-      }
+    getProductsFromFile(products => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
         console.log(err);
@@ -24,17 +38,8 @@ module.exports = class Product {
     });
   }
 
+  // Is called from controller end gets CB function from there which returns roduct List Page in Shop.
   static fetchAll(cb) {
-    const p = path.join(
-      path.dirname(require.main.filename),
-      "data",
-      "products.json"
-    );
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        cb([]);
-      }
-      cb(JSON.parse(fileContent));
-    });
+    getProductsFromFile(cb);
   }
-}
+};
