cfe7a119b63ac149d3b070541e0302f5e4e3cc61
Daniel Bauer <dan717@gmx.de>
2023-03-21 10:23:15 +0100

    Done editing a product

--- 09. A - Dynamic Routes & Advanced Models/controllers/admin.js
+++ 09. A - Dynamic Routes & Advanced Models/controllers/admin.js
@@ -17,7 +17,7 @@ exports.postAddProduct = (req, res, next) => {
   const price = req.body.price;
   const description = req.body.description;
 
-  const product = new Product(title, imageUrl, price, description);
+  const product = new Product(null, title, imageUrl, price, description);
   product.save();
   res.redirect("/products");
 };
@@ -51,7 +51,14 @@ exports.getEditProduct = (req, res, next) => {
 
 // Updating a product:
 exports.postEditProduct = (req, res, next) => {
-
+  const prodId = req.body.prodId;
+  const updatedTitle = req.body.title;
+  const updatedImageUrl = req.body.imageUrl;
+  const updatedPrice = req.body.price;
+  const updatedDesc = req.body.description;
+  const updatedProduct = new Product(prodId, updatedTitle, updatedImageUrl, updatedPrice, updatedDesc);
+  updatedProduct.save();
+  res.redirect("/admin/products")
 };
 
 exports.getProducts = (req, res, next) => {
