d94e7a1bdcf5b41c9781c2a717e7bbd51cebc666
Daniel Bauer <dan717@gmx.de>
2023-03-27 09:09:10 +0200

    Done connecting

--- 11. A - Understanding Sequelize/util/database.js
+++ 11. A - Understanding Sequelize/util/database.js
@@ -1,11 +1,8 @@
-const mysql = require("mysql2");
+const Sequelize = require("sequelize");
 
-// Pool for reusable connections to database
-const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  database: "node-complete",
-  password: "8m%V1h&o*vO5mabu",
+const sequelize = new Sequelize("node-complete", "root", "8m%V1h&o*vO5mabu", {
+  dialect: "msql",
+  host: "localhost"
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
