3d0efd1766d1a24ca8bb5368ee300a85c04410d7
Daniel Bauer <dan717@gmx.de>
2023-04-02 12:36:05 +0200

    Done insttalling mongodb driver

--- 12. A - Working with NoSQL & Using MongoDB/app.js
+++ 12. A - Working with NoSQL & Using MongoDB/app.js
@@ -4,13 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
-const sequelize = require("./util/database");
-const Product = require("./models/product");
-const User = require("./models/user");
-const Cart = require("./models/cart");
-const CartItem = require("./models/cart-item");
-const Order = require("./models/order");
-const OrderItem = require("./models/order-item");
+const mongoConnect = require("./util/database");
 
 // Making use of express
 const app = express();
@@ -19,10 +13,10 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-// Importing the Admin Routes:
-const adminRoutes = require("./routes/admin");
-// Importing the Shop Routes:
-const shopRoutes = require("./routes/shop");
+// // Importing the Admin Routes:
+// const adminRoutes = require("./routes/admin");
+// // Importing the Shop Routes:
+// const shopRoutes = require("./routes/shop");
 
 // Middleware Parsing:
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -31,59 +25,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Middleware to store user in request
 app.use((req, res, next) => {
-  User.findByPk(1)
-    .then(user => {
-      req.user = user;
-      next();
-    })
-    .catch(err => {console.log(err)})
+  // User.findByPk(1)
+  //   .then(user => {
+  //     req.user = user;
+  //     next();
+  //   })
+  //   .catch(err => {console.log(err)})
 });
 
-// Making use of Route Object adminRoutes:
-app.use('/admin', adminRoutes);
-// Making use of Route Object shopRoutes:
-app.use(shopRoutes);
-// Catch-All Middleware for errors:
-app.use(errorController.get404);
+// // Making use of Route Object adminRoutes:
+// app.use('/admin', adminRoutes);
+// // Making use of Route Object shopRoutes:
+// app.use(shopRoutes);
+// // Catch-All Middleware for errors:
+// app.use(errorController.get404);
 
-// Setting up relation a admin-user created a product: 1:n / n:1
-Product.belongsTo(User, {constraints: true, onDelete: "CASCADE" });
-User.hasMany(Product);
-// Setting up relations between user and cart: 1:1
-User.hasOne(Cart);
-Cart.belongsTo(User);
-// Setting up relation between cart and product. Needs join-table CartItem n:n
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
-// Setting up relation between user and order. 1:n
-Order.belongsTo(User);
-User.hasMany(Order);
-// Setting up relation between order and product. Needs join-table OrderItem n:n
-Order.belongsToMany(Product, { through: OrderItem });
-Product.belongsToMany(Order, { through: OrderItem });
-
-// Syncing sequelize to database
-// returning dummy user  or  creating it
-// server listening in case of no error
-sequelize
-  // .sync({ force: true })
-  .sync()
-  .then(result => {
-    return User.findByPk(1);
-    // console.log(result);
-  })
-  .then(user => {
-    if (!user) {
-      return User.create({ name: "Daniel", email: "test@test.com" })
-    }
-    return user;
-  })
-  .then(user => {
-    user.createCart()
-  })
-  .then(cart => {
-    app.listen(3000);
-  })
-  .catch(err => {
-    console.log(err);
+mongoConnect((client) => {
+  console.log(client);
+  app.listen(3000);
 });
--- 12. A - Working with NoSQL & Using MongoDB/util/database.js
+++ 12. A - Working with NoSQL & Using MongoDB/util/database.js
@@ -1,8 +1,13 @@
-const Sequelize = require("sequelize");
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize("node-complete", "root", "8m%V1h&o*vO5mabu", {
-  dialect: "mysql",
-  host: "localhost"
-});
+const mongoConnect = (callback) => {
+  MongoClient.connect("mongodb+srv://Daniel:nJcHhiLEQHUQqSVd@cluster0.igzmsfw.mongodb.net/?retryWrites=true&w=majority")
+  .then(client => {
+    console.log("Connected!");
+    callback(client);
+  })
+  .catch(err => console.log(err));
+};
 
-module.exports = sequelize;
+module.exports = mongoConnect;
