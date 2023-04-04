45494df388a8fd4b7e2afdcb058446948531c477
Daniel Bauer <dan717@gmx.de>
2023-04-04 11:28:22 +0200

    Done correcction for id conversion

--- 12. A - Working with NoSQL & Using MongoDB/models/product.js
+++ 12. A - Working with NoSQL & Using MongoDB/models/product.js
@@ -8,7 +8,7 @@ class Product {
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
-    this._id = id; //
+    this._id = new  mongodb.ObjectId(id);
   }
 
   // Either saves new product or updates existing one
@@ -21,7 +21,7 @@ class Product {
     if (this._id) {
       // Updates existing product
       dbOp = db.collection("products")
-      .updateOne({ _id: new mongodb.ObjectId(this._id) }, { $set: this });
+      .updateOne({ _id: this._id }, { $set: this });
     } else {
       // Saves new product
       dbOp = db.collection("products")
