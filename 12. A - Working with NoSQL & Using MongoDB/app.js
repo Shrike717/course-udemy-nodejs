d2fab059ec83301612b5dbe3ae59c373a12d7100
Daniel Bauer <dan717@gmx.de>
2023-04-05 16:58:32 +0200

    Done adding add to cart functionality

--- 12. A - Working with NoSQL & Using MongoDB/app.js
+++ 12. A - Working with NoSQL & Using MongoDB/app.js
@@ -28,7 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById("642c212665d957affe547b83")
     .then(user => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch(err => {console.log(err)})
