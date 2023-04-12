8e9c25ea13f65da256180a51b8c1139f6e4b2f89
Daniel Bauer <dan717@gmx.de>
2023-04-12 11:58:44 +0200

    done adding products to cart

--- 13. A - Working with Mongoose/models/user.js
+++ 13. A - Working with Mongoose/models/user.js
@@ -14,7 +14,11 @@ const userSchema = new Schema({
   cart: {
     items: [
       {
-        productId: { type: mongoose.SchemaTypes.ObjectId, ref: "Product", required: true },
+        productId: {
+          type: mongoose.SchemaTypes.ObjectId,
+          ref: "Product",
+          required: true,
+        },
         quantity: { type: Number, required: true },
       },
     ],
@@ -24,6 +28,32 @@ const userSchema = new Schema({
     },
   },
 });
+
+userSchema.methods.addToCart = function (product) {
+  //Checks whether added product is already in cart by finding index
+  const cartProductIndex = this.cart.items.findIndex((cp) => {
+    return cp.productId.toString() === product._id.toString(); // Strict comparison not workng with ObjectId type. Therefore converting to strings
+  });
+  let newQuantity = 1; // Initialize new quantity
+  const updatedCartItems = [...this.cart.items]; // Copying all items from cart array to work with for updating
+
+  if (cartProductIndex >= 0) {
+    // If Index is anything then -1  = product exists already in cart
+    newQuantity = this.cart.items[cartProductIndex].quantity + 1; // Incrementing qty on found product based  on old qty
+    updatedCartItems[cartProductIndex].quantity = newQuantity; // Either updating qty of existing product with incemented qty
+  } else {
+    updatedCartItems.push({
+      productId: product._id, // Mongoose automatcally wraps the id to an ObjectId
+      quantity: newQuantity,
+    }); // Or adding new product to cart with qty 1
+  }
+
+  const updatedCart = { items: updatedCartItems }; // Updates embedded Documemt carts in users collection
+
+  // Saving recent cart to DB
+  this.cart = updatedCart;
+  return this.save(); // Saves with build-in method of MG.
+};
 
 module.exports = mongoose.model("User", userSchema);
 
