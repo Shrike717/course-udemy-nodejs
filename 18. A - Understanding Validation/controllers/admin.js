1c40692114b088eae523eb41d22dbf3a1a71cddc
Daniel Bauer <dan717@gmx.de>
2023-04-25 13:39:46 +0200

    Initial push module 18 validation

---
+++ 18. A - Understanding Validation/controllers/admin.js
@@ -0,0 +1,138 @@
+const Product = require("../models/product");
+
+// Shows Admin Products page
+exports.getProducts = (req, res, next) => {
+	Product.find({ userId: req.user.id })
+		// .select("title price -_id") // Utility methods to fetch only certain data fields
+		// .populate("userId", "name")
+		.then((products) => {
+			// Path seen from views folder defined in ejs
+			res.render("admin/products", {
+				prods: products,
+				pageTitle: "Admin Products",
+				path: "/admin/products",
+			});
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+};
+
+// Returns Add/Edit Product form page:
+exports.getAddProduct = (req, res, next) => {
+	if (!req.session.isLoggedIn) {
+		return res.redirect("/login");
+	}
+	// Path seen from views folder defined in ejs
+	res.render("admin/edit-product", {
+		pageTitle: "Add Product",
+		path: "/admin/add-product",
+		editing: false,
+	});
+};
+
+// Adds a product by instanciating it from Product Model
+exports.postAddProduct = (req, res, next) => {
+	const title = req.body.title;
+	const price = req.body.price;
+	const description = req.body.description;
+	const imageUrl = req.body.imageUrl;
+	const product = new Product({
+		title: title,
+		price: price,
+		description: description,
+		imageUrl: imageUrl,
+		userId: req.user,
+	});
+	product
+		.save() // Save Method automatically given by Mongoose
+		.then((result) => {
+			// console.log(result);
+			console.log("Created Product");
+			res.redirect("/admin/products");
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+};
+
+// Step 1: Gets product which shall be edited and returns pre-populated Edit Product page:
+exports.getEditProduct = (req, res, next) => {
+	// Checks for optional data in query parameters:
+	// query object is automatically given by express.
+	// edit is in this case the key of the key-value pair in query parameter
+	const editMode = req.query.edit;
+
+	// Getting the product id
+	const prodId = req.params.productId;
+	// Receiving product with this id and rendering edit product page if there is a product:
+
+	// Gets the product and renders edit form page if there is one
+	Product.findById(prodId)
+		.then((product) => {
+			if (!product) {
+				res.redirect("/");
+			}
+			// Path seen from views folder defined in ejs
+			res.render("admin/edit-product", {
+				pageTitle: "Edit Product",
+				path: "/admin/edit-product",
+				editing: editMode,
+				product: product,
+			});
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+};
+
+// Step 2: Updating a product by click on Update button and saving it to DB:
+// First saves updated values from body in post request
+exports.postEditProduct = (req, res, next) => {
+	const prodId = req.body.productId;
+	const updatedTitle = req.body.title;
+	const updatedPrice = req.body.price;
+	const updatedDesc = req.body.description;
+	const updatedImageUrl = req.body.imageUrl;
+
+	// Finding product with id. Therefore updating product!
+	Product.findById(prodId)
+		.then((product) => {
+			if (product.userId.toString() !== req.user._id.toString()) {
+				// Check if user is allowed to edit this product
+				return res.redirect("/");
+			}
+			// Mapping updated values
+			(product.title = updatedTitle),
+				(product.price = updatedPrice),
+				(product.description = updatedDesc),
+				(product.imageUrl = updatedImageUrl);
+			return product
+				.save() // Loaded product (full mongoose-object with functions) will be updated by save method.
+				.then((result) => {
+					console.log("Updated Product!");
+					res.redirect("/admin/products");
+				});
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+};
+
+// Deleting a product:
+exports.postDeleteProduct = (req, res, next) => {
+	const prodId = req.body.productId;
+	Product.deleteOne({ _id: prodId, userId: req.user._id })
+		.then((result) => {
+			Product.findById(prodId).then((product) => {
+				// My check if product was deleted
+				if (!product) {
+					console.log("Deleted Product!");
+					res.redirect("/admin/products");
+				}
+			});
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+};
---
+++ 18. A - Understanding Validation/controllers/auth.js
@@ -0,0 +1,251 @@
+const crypto = require("crypto");
+
+const bcrypt = require("bcryptjs");
+require("dotenv").config();
+
+const User = require("../models/user");
+
+const sgMail = require("@sendgrid/mail");
+const { CLIENT_RENEG_LIMIT } = require("tls");
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+// Getting and displaying them on Login Page
+exports.getLogin = (req, res, next) => {
+	let message = req.flash("error");
+	if (message.length > 0) {
+		message = message[0];
+	} else {
+		message = null;
+	}
+	res.render("auth/login", {
+		path: "/login",
+		pageTitle: "Login",
+		errorMessage: message,
+	});
+};
+
+exports.getSignup = (req, res, next) => {
+	let message = req.flash("error");
+	if (message.length > 0) {
+		message = message[0];
+	} else {
+		message = null;
+	}
+	res.render("auth/signup", {
+		path: "/signup",
+		pageTitle: "Signup",
+		errorMessage: message,
+	});
+};
+
+// Handling post login request
+exports.postLogin = (req, res, next) => {
+	const email = req.body.email;
+	const password = req.body.password;
+	User.findOne({ email: email })
+		.then((user) => {
+			if (!user) {
+				req.flash("error", "Invalid email or passsword!");
+				return res.redirect("/login");
+			}
+			bcrypt
+				.compare(password, user.password)
+				.then((doMatch) => {
+					if (doMatch) {
+						req.session.isLoggedIn = true;
+						req.session.user = user;
+						return req.session.save((err) => {
+							console.log(err);
+							res.redirect("/");
+						});
+					}
+					req.flash("error", "Invalid email or passsword!");
+					res.redirect("/login");
+				})
+				.catch((err) => {
+					console.log(err);
+					res.redirect("/login");
+				});
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+};
+
+// Handles Signup and checks if email already exists.
+// If yes, redirects to signup page again
+// If no, creates new user with credenials and redirects to login page
+exports.postSignup = (req, res, next) => {
+	const email = req.body.email;
+	const password = req.body.password;
+	const confirmPassword = req.body.confirmPassword;
+	User.findOne({ email: email })
+		.then((userDoc) => {
+			if (userDoc) {
+				req.flash("error", "E-mail already exists!");
+				return res.redirect("/signup");
+			}
+			return bcrypt
+				.hash(password, 12)
+				.then((hashedPassword) => {
+					const user = new User({
+						email: email,
+						password: hashedPassword,
+						cart: { items: [] },
+					});
+					return user.save();
+				})
+				.then((result) => {
+					res.redirect("/login");
+					// Configurating Message and calling the send function on sgMail object with message afterwards
+					const msg = {
+						to: email, // Change to your recipient -> dan717@gmx.de
+						from: "dbauer.webdev@gmail.com", // Change to your verified sender
+						subject: "Signup succeeded!",
+						text: "You successfully signed up!",
+						html: "<strong>You successfully signed up!</strong>",
+					};
+					sgMail
+						.send(msg)
+						.then((response) => {
+							console.log(response[0].statusCode);
+							console.log(response[0].headers);
+						})
+						.catch((error) => {
+							console.error(error);
+						});
+				});
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+};
+
+// Handling post logout request
+exports.postLogout = (req, res, next) => {
+	req.session.destroy((err) => {
+		// Deletes session in DB
+		console.log(err);
+		res.redirect("/");
+	});
+};
+
+// Getting and displaying the Reset Page
+exports.getReset = (req, res, next) => {
+	let message = req.flash("error");
+	if (message.length > 0) {
+		message = message[0];
+	} else {
+		message = null;
+	}
+	res.render("auth/reset", {
+		path: "/reset",
+		pageTitle: "Reset Password",
+		errorMessage: message,
+	});
+};
+
+// When button Reset Password is clicked
+exports.postReset = (req, res, next) => {
+	// First creating security token with build in module crypto
+	crypto.randomBytes(32, (err, buffer) => {
+		if (err) {
+			console.log(err);
+			return res.redirect("/reset");
+		}
+		const token = buffer.toString("hex"); // Needs hex to be converted to ASCII
+		User.findOne({ email: req.body.email })
+			.then((user) => {
+				if (!user) {
+					req.flash("error", "No account with that email found!");
+					return res.redirect("/reset");
+				}
+				user.resetToken = token; // Setting token to user object
+				user.resetTokenExpiration = Date.now() + 3600000; // Setting date + 1 hour
+				return user.save();
+			})
+			.then((result) => {
+				res.redirect("/");
+				const msg = {
+					to: req.body.email, // Extracting email from reset form -> dan717@gmx.de
+					from: "dbauer.webdev@gmail.com", // Change to your verified sender -> our shop
+					subject: "Password reset",
+					text: "You successfully signed up!",
+					html: `
+                        <p>You requested a passsword reset<p>
+                        <p>Click this <a href="http://localhost:3000/reset/${token}">link</a> to set a new password<p>
+                    `,
+				};
+				sgMail
+					.send(msg)
+					.then((response) => {
+						console.log(response[0].statusCode);
+						console.log(response[0].headers);
+					})
+					.catch((error) => {
+						console.error(error);
+					});
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	});
+};
+
+// Getting and displaying the New Password Page after click on link in email
+exports.getNewPassword = (req, res, next) => {
+	const token = req.params.token;
+	User.findOne({
+		resetToken: token,
+		resetTokenExpiration: { $gt: Date.now() },
+	})
+		.then((user) => {
+			let message = req.flash("error");
+			if (message.length > 0) {
+				message = message[0];
+			} else {
+				message = null;
+			}
+			res.render("auth/new-password", {
+				path: "/new-password",
+				pageTitle: "New Password",
+				errorMessage: message,
+				userId: user._id.toString(),
+				passwordToken: token,
+			});
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+};
+
+// Setting new password for user after click on Update Password Button
+exports.postNewPassword = (req, res, next) => {
+    const newPassword = req.body.password;
+	const userId = req.body.userId;
+	const passwordToken = req.body.passwordToken;
+	let resetUser;
+
+	User.findOne({
+		resetToken: passwordToken,
+		resetTokenExpiration: { $gt: Date.now() }, // Token date must be greater then actual date
+		_id: userId,
+	})
+		.then((user) => {
+            console.log(user);
+			resetUser = user;
+			return bcrypt.hashSync(newPassword, 12); // Encrypting new password
+		})
+		.then((hashedPassword) => {
+			resetUser.password = hashedPassword;
+			resetUser.resetToken = undefined; // Setting token back to undefined
+			resetUser.resetTokenExpiration = undefined; // Setting expiration back to undefined
+			return resetUser.save();
+		})
+		.then((result) => {
+			res.redirect("/login");
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+};
