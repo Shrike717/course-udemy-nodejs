92d13006c1896257d0176790913e020d9ca0ddb8
Daniel Bauer <dan717@gmx.de>
2023-04-26 12:19:38 +0200

    Done adding password validation

--- 18. A - Understanding Validation/routes/auth.js
+++ 18. A - Understanding Validation/routes/auth.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { check } = require("express-validator");
+const { check, body } = require("express-validator");
 
 const authController = require("../controllers/auth");
 
@@ -14,15 +14,23 @@ router.post("/login", authController.postLogin);
 
 router.post(
 	"/signup",
-	check("email")
-		.isEmail()
-		.withMessage("Please enter a valid email.")
-		.custom((value, { req }) => {
-			if (value === "test@test.com") {
-				throw new Error("This email adress iss forbidden!");
-			}
-            return true;
-		}),
+	[
+		check("email")
+			.isEmail()
+			.withMessage("Please enter a valid email.")
+			.custom((value, { req }) => {
+				if (value === "test@test.com") {
+					throw new Error("This email adress iss forbidden!");
+				}
+				return true;
+			}),
+		body(
+			"password",
+			"Please enter a password with only numbers and text with at least 5 characters!"
+		)
+			.isLength({ min: 5 })
+			.isAlphanumeric(),
+	],
 	authController.postSignup
 );
 
