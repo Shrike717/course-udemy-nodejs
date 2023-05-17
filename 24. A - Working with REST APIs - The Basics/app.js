5c31b54418f9643c57dd9f1d861a9c2635997186
Daniel Bauer <dan717@gmx.de>
2023-05-17 09:23:50 +0200

    Done working with postman

--- 24. A - Working with REST APIs - The Basics/app.js
+++ 24. A - Working with REST APIs - The Basics/app.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const feedRoutes = require("./routes/feed");
 
 const app = express();
+
+// Parsing incoming JSON data from request bodies (applicaton/json)
+app.use(bodyParser.json());
 
 app.use("/feed", feedRoutes);
 
--- 24. A - Working with REST APIs - The Basics/routes/feed.js
+++ 24. A - Working with REST APIs - The Basics/routes/feed.js
@@ -7,4 +7,7 @@ const router = express.Router(); // Creating the routr
 // GET /feed/posts will be handled. There is a pre-filter "/feed" in app.js
 router.get("/posts", feedController.getPosts);
 
+// POST /feed/post
+router.post("/post", feedController.createPost);
+
 module.exports = router;
