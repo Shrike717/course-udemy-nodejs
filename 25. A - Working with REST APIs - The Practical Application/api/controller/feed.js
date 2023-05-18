c4c5581a4eb0381b9a03fd69109fedaeabbd42cd
Daniel Bauer <dan717@gmx.de>
2023-05-18 12:27:01 +0200

    Done fetching lis of posts

--- 25. A - Working with REST APIs - The Practical Application/api/controller/feed.js
+++ 25. A - Working with REST APIs - The Practical Application/api/controller/feed.js
@@ -1,13 +1,24 @@
 exports.getPosts = (req, res, next) => {
 	res.status(200).json({
-		posts: [{ title: "First post", content: "This is my first post!" }],
+		posts: [
+			{
+                _id: "1",
+				title: "First post",
+				content: "This is my first post!",
+				imageUrl: "images/Book.jpg",
+				creator: {
+                    name: "Daniel Bauer"
+                },
+				createdAt: new Date(),
+			},
+		],
 	});
 };
 
 exports.createPost = (req, res, next) => {
 	const title = req.body.title;
 	const content = req.body.content;
-    console.log(title, content);
+	console.log(title, content);
 
 	// Create post in db
 	res.status(201).json({
