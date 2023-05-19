f2403e6fd9fcaa8556efe6ab39fcd381a52dcd0b
Daniel Bauer <dan717@gmx.de>
2023-05-19 12:54:38 +0200

    Done adding basic server sidee validation

--- 25. A - Working with REST APIs - The Practical Application/api/controller/feed.js
+++ 25. A - Working with REST APIs - The Practical Application/api/controller/feed.js
@@ -1,3 +1,5 @@
+const { validationResult } = require("express-validator");
+
 exports.getPosts = (req, res, next) => {
 	res.status(200).json({
 		posts: [
@@ -16,6 +18,17 @@ exports.getPosts = (req, res, next) => {
 };
 
 exports.createPost = (req, res, next) => {
+	// Errors the validator package might have gathered
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		return res
+			.status(422)
+			.json({
+				message: "Validation failed! Entered data is incorrect.",
+				errors: errors.array(),
+			});
+	}
+
 	const title = req.body.title;
 	const content = req.body.content;
 	console.log(title, content);
@@ -30,7 +43,7 @@ exports.createPost = (req, res, next) => {
 			creator: {
 				name: "Daniel Bauer",
 			},
-            createdAt: new Date(),
+			createdAt: new Date(),
 		},
 	});
 };
