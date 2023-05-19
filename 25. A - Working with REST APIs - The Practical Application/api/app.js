7d4fcc87fa6e5c55c0240afe6a5c970200d1c2cc
Daniel Bauer <dan717@gmx.de>
2023-05-19 14:34:12 +0200

    Done connecting with mongoose and creating a  post schema and model

--- 25. A - Working with REST APIs - The Practical Application/api/app.js
+++ 25. A - Working with REST APIs - The Practical Application/api/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const feedRoutes = require("./routes/feed");
 
@@ -10,12 +12,26 @@ app.use(bodyParser.json());
 
 // Setting headers to avoid CORS error to every response before requests are routed further in our app
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    next();
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader(
+		"Access-Control-Allow-Methods",
+		"GET, POST, PUT, PATCH, DELETE"
+	);
+	res.setHeader(
+		"Access-Control-Allow-Headers",
+		"Content-Type, Authorization"
+	);
+	next();
 });
 
 app.use("/feed", feedRoutes);
 
-app.listen(8080);
+// Connecting to Mongo Db with Mongoose:
+mongoose
+	.connect(process.env.DB_URI)
+	.then((result) => {
+		app.listen(8080);
+	})
+	.catch((err) => {
+		console.log(err);
+	});
