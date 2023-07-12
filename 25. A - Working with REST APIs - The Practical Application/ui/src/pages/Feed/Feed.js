1ea34e14e75a28a5ada917146f53c4fcbb493b5c
Daniel Bauer <dan717@gmx.de>
2023-07-12 10:37:03 +0200

    Done pushin updated posts with websocket o all connected clients

--- 25. A - Working with REST APIs - The Practical Application/ui/src/pages/Feed/Feed.js
+++ 25. A - Working with REST APIs - The Practical Application/ui/src/pages/Feed/Feed.js
@@ -50,10 +50,13 @@ class Feed extends Component {
 		socket.on("posts", (data) => {
 			if (data.action === "create") {
 				this.addPost(data.post);
+			} else if (data.action === "update") {
+				this.updatePost(data.post);
 			}
 		});
 	}
 
+	// Renders new post immediately without reloading page
 	addPost = (post) => {
 		this.setState((prevState) => {
 			const updatedPosts = [...prevState.posts];
@@ -66,6 +69,22 @@ class Feed extends Component {
 			return {
 				posts: updatedPosts,
 				totalPosts: prevState.totalPosts + 1,
+			};
+		});
+	};
+
+	// Renders updated post immediately without reloading page
+	updatePost = (post) => {
+		this.setState((prevState) => {
+			const updatedPosts = [...prevState.posts];
+			const updatedPostIndex = updatedPosts.findIndex(
+				(p) => p._id === post._id
+			);
+			if (updatedPostIndex > -1) {
+				updatedPosts[updatedPostIndex] = post;
+			}
+			return {
+				posts: updatedPosts,
 			};
 		});
 	};
@@ -205,17 +224,17 @@ class Feed extends Component {
 					createdAt: resData.post.createdAt,
 				};
 				this.setState((prevState) => {
-					let updatedPosts = [...prevState.posts];
-					if (prevState.editPost) {
-						const postIndex = prevState.posts.findIndex(
-							(p) => p._id === prevState.editPost._id
-						);
-						updatedPosts[postIndex] = post;
-						// } else if (prevState.posts.length < 2) { // This loads a new post 2 times with websocket
-						// 	updatedPosts = prevState.posts.concat(post);
-					}
+					// let updatedPosts = [...prevState.posts];
+					// if (prevState.editPost) {
+					// 	const postIndex = prevState.posts.findIndex(
+					// 		(p) => p._id === prevState.editPost._id
+					// 	);
+					// 	updatedPosts[postIndex] = post;
+					// } else if (prevState.posts.length < 2) { // This loads a new post 2 times with websocket
+					// 	updatedPosts = prevState.posts.concat(post);
+					// }
 					return {
-						posts: updatedPosts,
+						// posts: updatedPosts,
 						isEditing: false,
 						editPost: null,
 						editLoading: false,
