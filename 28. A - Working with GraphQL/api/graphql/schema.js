c424d60c8ddd8b67241ee1395da05cc19e6d11fc
Daniel Bauer <dan717@gmx.de>
2023-07-14 08:32:39 +0200

    Done adding mutaton schema for creating user

---
+++ 28. A - Working with GraphQL/api/graphql/schema.js
@@ -0,0 +1,39 @@
+// Allows to build schema which than gets parsed
+const { buildSchema } = require("graphql");
+
+// Funcion that returns the schema object this will generate
+module.exports = buildSchema(`
+    type Post {
+        _id: ID!
+        title: String!
+        content: String!
+        imageUrl: String!
+        creator: User!
+        createdAt: String!
+        updatedAt: String!
+
+    }
+
+    type User {
+        _id: ID!
+        name: String!
+        email: String!
+        password: String
+        status: String!
+        posts: [Post!]!
+    }
+
+    input UserInputData {
+        email: String!
+        name: String!
+        password: String!
+    }
+
+    type RootMutation {
+        createUser(userInput: UserInputData): User!
+    }
+
+    schema {
+        mutation: RootMutation
+    }
+`);
