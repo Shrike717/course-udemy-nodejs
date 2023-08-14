0ee1ab3de282a5e01bdfaf3796850f4a1533aa76
Daniel Bauer <dan717@gmx.de>
2023-08-14 11:54:11 +0200

    Done setting up a test daatabase

--- 30. A - Testing Node.js Applications/test/auth-controller.js
+++ 30. A - Testing Node.js Applications/test/auth-controller.js
@@ -1,6 +1,7 @@
 const jwt = require("jsonwebtoken");
 const sinon = require("sinon"); // Package for stubbing external functions
 require("dotenv").config();
+const mongoose = require("mongoose");
 
 //  Importing expect function from chai package
 const expect = require("chai").expect;
@@ -39,5 +40,30 @@ describe("Auth Controller-Login", () => {
 
 		// Restoring findOne method again
 		User.findOne.restore();
+	});
+
+	// Testing if we get a response with a valid user status:
+	// Requuires  done keyword because communicating with DB is async code
+	it("should return a response with a valid user status for an existing user", (done) => {
+		// First we have to setup a connection to our test-DB
+		mongoose
+			.connect(process.env.DB_URI_TEST, {
+				useNewUrlParser: true,
+				useUnifiedTopology: true,
+			})
+			.then((result) => {
+				// Defining the testin logic:
+				// First we need a dummy user
+				const user = new User({
+					email: "test@test.com",
+					passsword: "test",
+					name: "Tester",
+					posts: [],
+				});
+				// Then saving the user to DB and return result as promise
+				return user.save();
+			})
+			.then(() => {})
+			.catch((err) => console.log(err));
 	});
 });
