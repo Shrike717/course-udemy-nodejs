7f7a0b0a45ae0ba01622b40cd6067d81b93b724c
Daniel Bauer <dan717@gmx.de>
2023-08-16 15:51:14 +0200

    Done llearning about the problem of not having gllobal variables like __dirname anymore in ES modules and how too construct them agaiin

--- 32. A - Modern JavaScript and NodeJS/response-handler.js
+++ 32. A - Modern JavaScript and NodeJS/response-handler.js
@@ -21,10 +21,26 @@
 // export default resHandler;
 
 // **** ES modules import and export: Export as named export with keyword export as single function: ****
-import fs from "fs"; // ES modules import
+// import fs from "fs"; // ES modules import
+
+// export const resHandler = (req, res, next) => {
+// 	fs.readFile("my-page.html", "utf8", (err, data) => {
+// 		res.send(data);
+// 	});
+// };
+
+// **** ES modules import and export: Constructing global variables again ****
+import path from "path";
+
+// We need this helpers to construct global variables like __dirname or __filename
+import * as url from "url";
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
 
 export const resHandler = (req, res, next) => {
-	fs.readFile("my-page.html", "utf8", (err, data) => {
-		res.send(data);
-	});
+	// fs.readFile("my-page.html", "utf8", (err, data) => {
+	// 	res.send(data);
+	// });
+	// We construct the path: __dirname is our actual folder, then we would need folder if any, then the filename
+	res.sendFile(path.join(__dirname, "my-page.html"));
 };
