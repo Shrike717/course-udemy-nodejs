1d6d81fb56737edd6fe510da6017c70a64d83a2f
Daniel Bauer <dan717@gmx.de>
2023-08-18 17:01:20 +0200

    Learned about union types

--- 33. A - NodeJS and TypeScript/app.ts
+++ 33. A - NodeJS and TypeScript/app.ts
@@ -1,21 +1,31 @@
-
 const num1Element = document.getElementById("num1") as HTMLInputElement;
 const num2Element = document.getElementById("num2") as HTMLInputElement;
 const buttonElement = document.querySelector("button")!;
 
-function add(num1: number, num2: number){
-    return num1 + num2
-   }
+function add(num1: number | string, num2: number | string) {
+	// Running different code for different types of inputs
+	if (typeof num1 === "number" && typeof num2 === "number") {
+		return num1 + num2;
+	} else if (typeof num1 === "string" && typeof num2 === "string") {
+		return num1 + " " + num2;
+	} else {
+		// For mixed types we force a conversion to a number
+		return +num1 + +num2;
+	}
+}
 
-   // Possibily 1 to check if there is a button element:
-   if (buttonElement) {
-        // Adding eventlistener:
-        buttonElement.addEventListener("click", () => {
-            // Now extacting the values of the input fields.
-            const num1 = num1Element.value;
-            const num2 = num2Element.value;
-            // Now passing the value of the function:
-            const result = add(+num1, +num2);
-            console.log(result);
-        })
-    }
+// Possibily 1 to check if there is a button element:
+if (buttonElement) {
+	// Adding eventlistener:
+	buttonElement.addEventListener("click", () => {
+		// Now extacting the values of the input fields.
+		const num1 = num1Element.value;
+		const num2 = num2Element.value;
+		// Now passing the value of the function:
+		const result = add(+num1, +num2);
+		// We also need to have the functon working with strings
+		const stringResult = add(num1, num2);
+		console.log(result);
+		console.log(stringResult);
+	});
+}
