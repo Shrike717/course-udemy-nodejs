2d2c050b5452ffe242e41838719a672e0af81bc6
Daniel Bauer <dan717@gmx.de>
2023-08-18 12:20:05 +0200

    Done llearningg about inferred types and type casting

--- 33. A - NodeJS and TypeScript/app.js
+++ 33. A - NodeJS and TypeScript/app.js
@@ -1,5 +1,15 @@
+var num1Element = document.getElementById("num1");
+var num2Element = document.getElementById("num2");
+var buttonElement = document.querySelector("button");
 function add(num1, num2) {
     return num1 + num2;
 }
-console.log(add(1, 5));
-console.log(add("1", "5"));
+// Adding eventlistener:
+buttonElement.addEventListener("click", function () {
+    // Now extacting the values of the input fields.
+    var num1 = num1Element.value;
+    var num2 = num2Element.value;
+    // Now passing the value of the function:
+    var result = add(num1, +num2);
+    console.log(result);
+});
