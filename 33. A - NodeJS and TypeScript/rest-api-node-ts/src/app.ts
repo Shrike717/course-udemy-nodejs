218daf7c5c7ef08cea5c709a0559b923c0236e47
Daniel Bauer <dan717@gmx.de>
2023-08-28 15:42:14 +0200

    Done learning about src and output folder and nodemon in ts

---
+++ 33. A - NodeJS and TypeScript/rest-api-node-ts/src/app.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+import todosRoutes from "./routes/todos";
+
+// Test
+
+const app = express();
+
+// MW for parsing Json:
+app.use(bodyParser.json());
+
+// MW for using the routes:
+app.use(todosRoutes);
+
+app.listen(3000);
