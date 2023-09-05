4b575a06938edab61d8649457b476ee2b020c511
Daniel Bauer <dan717@gmx.de>
2023-09-05 10:32:21 +0200

    Done creating a simple weebsserver with nod  as comparison

--- 34. A - An Introduction to Deno/app.js
+++ 34. A - An Introduction to Deno/app.js
@@ -1,10 +1,27 @@
-// This is a comparison of ccreating a file in node and writin to it:
+// // This is a comparison of creating a file in node and writing to it:
 
-// We import the file system from the promise part of the core module
-const fs = require("fs").promises;
+// // We import the file system from the promise part of the core module
+// const fs = require("fs").promises;
 
-const text = "This is a test and should be stored in a file!";
+// const text = "This is a test and should be stored in a file!";
 
-fs.writeFile("node-message.text", text).then(() => {
-	console.log("Wrrote to file");
+// fs.writeFile("node-message.text", text).then(() => {
+// 	console.log("Wrrote to file");
+// });
+
+// ****************************************************************
+
+// This is a comparison of creating a Node server
+
+// First we have  to import the http functionality
+const http = require("http");
+
+// Then createServer() needs a function with gets the incoming rquest an the rsponse object and  will be executed
+// for every incoming request
+const server = http.createServer((req, res) => {
+	// then we call the end() method on the response object and send it back
+	res.end("Hello World (From Node)!");
 });
+
+// To now spin up the above created server we need to listen to requests at a port
+server.listen(3000);
