e7511c673f5d4e8f10b1c65e0f8865d352ab4fff
Daniel Bauer <dan717@gmx.de>
2023-09-06 17:23:06 +0200

    Initial push module 35 Deno CRUD MongoDB

---
+++ 35. A - Deno, CRUD & Databases (MongoDB)/deno/app.ts
@@ -0,0 +1,22 @@
+// This is a server with Oak:
+import { Application } from "https://deno.land/x/oak/mod.ts";
+
+// Importing the router
+import todosRoutes from "./routes/todos.ts";
+
+// Here we instanciate a new application from a class
+const app = new Application();
+
+// Test Middleware to show the async problem when using next:
+app.use(async (ctx, next) => {
+	console.log("Test middleware!");
+	await next();
+});
+
+// Here we register the routes. Important is the routes() method on todosRoutes object
+app.use(todosRoutes.routes());
+// We need to register a socoond MW for the routes:This is needed to handle routes properly in Oak
+app.use(todosRoutes.allowedMethods());
+
+// Then listening on a port with top level await
+await app.listen({ port: 3000 });
